7798505896de738f03235cbed03ac90ba6c3e5af
Greg Osha <gregory.osha@gmail.com>
2023-10-22 18:40:28 -0400

    super fast

--- shaft_cut.py
+++ shaft_cut.py
@@ -48,7 +48,7 @@ def main() -> None:
         if not isinstance(dist, int):
             raise TypeError('Number of steps must be an int')
         else:
-            push(dist, 0.001)
+            push(dist, 0.0001)
     
 
 def pin_setup() -> None:
